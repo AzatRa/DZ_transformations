0cd68d5ad85640e5ee461da1abd94352e6f245b7
Азат Ра <azat.ra.xim@mail.ru>
2025-01-12 00:06:36 +0500

    Add files via upload

--- Assets/SizeChanger.cs
+++ Assets/SizeChanger.cs
@@ -5,18 +5,15 @@ public class SizeChanger : MonoBehaviour
     [SerializeField] private float _sizingSpeed;
     [SerializeField] private float _maxSize;
 
-    private float inverter = -1f;
-    private float minSize = 1f;
+    private float _inverter = -1f;
+    private float _minSize = 1f;
 
-    void Update()
+    private void Update()
     {
-        if (transform.localScale.x < minSize || transform.localScale.y < minSize || transform.localScale.z < minSize)
+        if (transform.localScale.x < _minSize || transform.localScale.y < _minSize || transform.localScale.z < _minSize ||
+            transform.localScale.x > _maxSize || transform.localScale.y > _maxSize || transform.localScale.z > _maxSize)
         {
-            _sizingSpeed *= inverter;
-        }
-        else if (transform.localScale.x > _maxSize ||  transform.localScale.y > _maxSize || transform.localScale.z > _maxSize)
-        {
-            _sizingSpeed *= inverter;
+            _sizingSpeed *= _inverter;
         }
 
         transform.localScale += new Vector3(_sizingSpeed, _sizingSpeed, _sizingSpeed) * Time.deltaTime;
--- Assets/YAxisRotator.cs
+++ Assets/YAxisRotator.cs
@@ -4,7 +4,7 @@ public class YAxisRotator : MonoBehaviour
 {
     [SerializeField] private float _yAxisRotationSpeed;
 
-    void Update()
+    private void Update()
     {
         transform.Rotate(0, _yAxisRotationSpeed * Time.deltaTime, 0);
     }
